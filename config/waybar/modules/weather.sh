aa8df2404163d5e275f7ab7ebdf37f7ccf0fe540
darch7 <monzondaniel554@gmail.com>
2021-06-04 17:27:22 -0300

    actualizacion

--- config/waybar/modules/weather.sh
+++ config/waybar/modules/weather.sh
@@ -36,37 +36,37 @@ temperature=$(echo ${weather[2]} | sed -E 's/([[:digit:]]+)\.\./\1 to /g')
 # https://fontawesome.com/icons?s=solid&c=weather
 case $(echo ${weather[1]##*,} | tr '[:upper:]' '[:lower:]') in
 "clear" | "sunny")
-    condition=""
+    condition="滛 "
     ;;
 "partly cloudy")
-    condition=""
+    condition=" "
     ;;
 "cloudy")
-    condition=""
+    condition=" "
     ;;
 "overcast")
-    condition=""
+    condition=" "
     ;;
 "mist" | "fog" | "freezing fog")
-    condition=""
+    condition=" "
     ;;
 "patchy rain possible" | "patchy light drizzle" | "light drizzle" | "patchy light rain" | "light rain" | "light rain shower" | "rain")
-    condition=""
+    condition=" "
     ;;
 "moderate rain at times" | "moderate rain" | "heavy rain at times" | "heavy rain" | "moderate or heavy rain shower" | "torrential rain shower" | "rain shower")
-    condition=""
+    condition=" "
     ;;
 "patchy snow possible" | "patchy sleet possible" | "patchy freezing drizzle possible" | "freezing drizzle" | "heavy freezing drizzle" | "light freezing rain" | "moderate or heavy freezing rain" | "light sleet" | "ice pellets" | "light sleet showers" | "moderate or heavy sleet showers")
-    condition=""
+    condition=" "
     ;;
 "blowing snow" | "moderate or heavy sleet" | "patchy light snow" | "light snow" | "light snow showers")
-    condition=""
+    condition=" "
     ;;
 "blizzard" | "patchy moderate snow" | "moderate snow" | "patchy heavy snow" | "heavy snow" | "moderate or heavy snow with thunder" | "moderate or heavy snow showers")
-    condition=""
+    condition=" "
     ;;
 "thundery outbreaks possible" | "patchy light rain with thunder" | "moderate or heavy rain with thunder" | "patchy light snow with thunder")
-    condition=""
+    condition=" "
     ;;
 *)
     condition=""
@@ -76,4 +76,4 @@ esac
 
 #echo $temp $condition
 
-echo -e "{\"text\":\""$temperature $condition"\", \"alt\":\""${weather[0]}"\", \"tooltip\":\""${weather[0]}: $temperature ${weather[1]}"\"}"
+echo -e "{\"text\":\""$condition $temperature"\", \"alt\":\""${weather[0]}"\", \"tooltip\":\""${weather[0]}: $temperature ${weather[1]}"\"}"
